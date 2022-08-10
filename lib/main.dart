f1108b78763856eaa8836b17d1115a6a3a56b14b
mopah emma <mopahemma@gmail.com>
2022-08-10 06:43:51 +0100

    iniitals for main

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'CreatoDisplay'
       ),
-      home: const ForgotPassword(),
+      home: const SplashScreen(),
     );
   }
 }
